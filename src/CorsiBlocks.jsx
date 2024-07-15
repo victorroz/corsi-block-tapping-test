f2166b97a67d42956b71bfba45fb97c8c92992da
Victor C Rozario <victorroz@hotmail.com>
2024-07-14 21:45:46 -0600

    feat: corsi block functionality

--- src/CorsiBlocks.jsx
+++ src/CorsiBlocks.jsx
@@ -1,22 +1,128 @@
+import { useState, useEffect } from "react";
 import { Container, SimpleGrid } from "@mantine/core";
+import useSound from "use-sound";
+import { saveAs } from "file-saver";
+import { useGlobalContext } from "./GlobalProvider";
+
 import { blocks } from "./data/blocks";
 import { sequences } from "./data/sequences";
+import go from "/go.mp3";
 
 const CorsiBlocks = () => {
+  const { participantId, setParticipantId } = useGlobalContext();
+  const [play] = useSound(go);
+  const numOfSequences = sequences.length;
+  const [activeSequenceIndex, setActiveSequenceIndex] = useState(0);
+  const [activeSequence, setActiveSequence] = useState([]);
+  const [sequenceIndex, setSequenceIndex] = useState(-1);
+  const [userSequence, setUserSequence] = useState([]);
+  const [isSequenceActive, setIsSequenceActive] = useState(false);
+  const [attempts, setAttempts] = useState(0);
+  const [rerunSequence, setRerunSequence] = useState(false);
+  const [selectionsRecord, setSelectionsRecord] = useState([]);
+  const [correctReplications, setCorrectReplications] = useState(1);
+
+  useEffect(() => {
+    setActiveSequence(sequences[activeSequenceIndex]);
+    setSequenceIndex(-1);
+    setUserSequence([]);
+    setIsSequenceActive(true);
+  }, [activeSequenceIndex]);
+
+  useEffect(() => {
+    if (activeSequence.length === 0) return;
+
+    let currentSeqIndex = 0;
+
+    const interval = setInterval(() => {
+      setSequenceIndex(activeSequence[currentSeqIndex]);
+      currentSeqIndex++;
+
+      if (currentSeqIndex >= activeSequence.length) {
+        clearInterval(interval);
+        setTimeout(() => {
+          setSequenceIndex(-1);
+          setIsSequenceActive(false);
+        }, 1000);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [activeSequence, rerunSequence]);
+
+  const handleBlockClick = (id) => {
+    if (isSequenceActive) return;
+
+    setUserSequence((prev) => [...prev, id]);
+  };
+
+  const handleClick = () => {
+    const isCorrect = userSequence.every(
+      (id, index) => id === activeSequence[index]
+    );
+    console.log(isCorrect ? "Correct sequence!" : "Incorrect sequence.");
+
+    setSelectionsRecord((prev) => [
+      ...prev,
+      {
+        shownSequence: [...activeSequence],
+        userSequence: [...userSequence],
+        isCorrect,
+      },
+    ]);
+
+    if (isCorrect) {
+      setAttempts(0);
+      setCorrectReplications((prev) => prev + 1);
+      setActiveSequenceIndex((prev) => (prev + 1) % numOfSequences);
+      play();
+    } else {
+      setAttempts((prev) => prev + 1);
+      if (attempts + 1 >= 2) {
+        console.log("Program stopped. Too many incorrect attempts.");
+        console.log(`Correctly replicated sequences: ${correctReplications}`);
+        handleDownload();
+        setParticipantId("");
+        return;
+      }
+
+      setRerunSequence((prev) => !prev);
+      setSequenceIndex(-1);
+      setIsSequenceActive(true);
+    }
+    setUserSequence([]);
+  };
+
+  const handleDownload = () => {
+    const content = `
+      Participant ID: ${participantId}
+      
+      Corsi Span: ${correctReplications}
+
+      Selections Record:
+      ${JSON.stringify(selectionsRecord, null, 2)}
+    `;
+    const blob = new Blob([content], { type: "text/plain;charset=utf-8" });
+    saveAs(blob, `${participantId}.txt`);
+  };
+
   return (
     <Container fluid>
       <SimpleGrid cols={5} spacing="xl" verticalSpacing="xl">
         {blocks.map((block) => (
           <div
             key={block.id}
-            className={
+            className={`${
               block.active ? "block-container" : "block-container-hidden"
-            }
+            } ${block.id === sequenceIndex ? "block-container-active" : ""}`}
+            onClick={() => handleBlockClick(block.id)}
           >
             {block.id}
           </div>
         ))}
-        <button className="button-container">Done</button>
+        <button className="button-container" onClick={handleClick}>
+          Done
+        </button>
       </SimpleGrid>
     </Container>
   );
